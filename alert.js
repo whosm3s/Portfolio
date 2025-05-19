db0b13eebe0ae54fc6f98cb5d056733c248d2152
Meseret Getachew <meseretgetachew090@gmail.com>
2025-05-19 15:24:13 +0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,20 @@
+function showAlertCSS() {
+
+    alert("CSS button have been clicked!");
+
+} 
+function showAlertHTML() {
+    alert("HTML button have been clicked!");
+} 
+function showAlertBOOTSTRAP() {
+    alert("BOOTSTRAP button have been clicked!");
+} 
+function showAlertRUBY() {
+    alert("RUBY button have been clicked!");
+} 
+function showAlertSEEPROJECT() {
+    alert("See Project button have been clicked!");
+} 
+function showAlertGETMYRESUME() {
+    alert("Ger My Resume button have been clicked!");
+} 
